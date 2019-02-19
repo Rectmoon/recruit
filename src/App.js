abe7f9dc4d7ef17e0fa3dc4c11b6e05f161f4cab
Rectmoon <1426890675@qq.com>
2019-02-19 08:39:53 +0800

    'ee'

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'antd-mobile/dist/antd-mobile.css'
+import { Button, List } from 'antd-mobile'
 
 class App extends Component {
   render() {
+    const boss = '李云龙'
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>独立团， 团长{boss}</h2>
+        <一营 laoda="张大彪" />
+        <骑兵连 laoda="孙德胜" />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+class 一营 extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      solders: ['虎子', '柱子', '王根生']
+    }
+  }
+
+  addSolder() {
+    this.setState({
+      solders: [
+        ...this.state.solders,
+        '新兵单子' + Math.random().toFixed(4) * 10000
+      ]
+    })
+  }
+
+  render() {
+    const boss = this.props.laoda
+    return (
+      <div>
+        <h2>一营营长，{boss}</h2>
+        <Button type="primary" onClick={() => this.addSolder()}>
+          招兵
+        </Button>
+        <h3>士兵</h3>
+        <List>
+          {this.state.solders.map((item, i) => (
+            <List.Item key={i}>{item}</List.Item>
+          ))}
+        </List>
+      </div>
+    )
+  }
+}
+
+function 骑兵连(props) {
+  return <h2>冲啊, 骑兵连连长{props.laoda}</h2>
+}
+
+export default App
